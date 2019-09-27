a15ab40ff860f008e396da82cee0fa07c693bd99
vagrant <vagrant@debian9.localdomain>
2019-09-27 08:04:13 -0700

    TODO : selection cycle pour l'ajout d'une saison

--- src/AppBundle/Form/SaisonType.php
+++ src/AppBundle/Form/SaisonType.php
@@ -6,9 +6,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use AppBundle\Form\CycleType;
 
 use AppBundle\Entity\Saison;
+use AppBundle\Entity\Cycle;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SaisonType extends AbstractType
@@ -17,7 +18,8 @@ class SaisonType extends AbstractType
     {
         $builder
             ->add('name', TextType::class, ['label' => 'Saison : '])
-            ->add('cycle', CycleType::class)
+            // TODO: faire une liste deroulante pour le choix du cycle
+            ->add('cycle', ChoiceType::class, [])
             ->add('save', SubmitType::class, ['label' => 'Enregistrer'])
         ;
     }
