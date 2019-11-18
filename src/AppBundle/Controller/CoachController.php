3493cd47a130fec4164ce34ce381db03cb97fd0a
vagrant <vagrant@debian>
2019-11-18 19:39:43 +0000

    Modification des routes

--- src/AppBundle/Controller/CoachController.php
+++ src/AppBundle/Controller/CoachController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 class CoachController extends Controller
 {
     /**
-     * @Route("/Coach/new", name="newCoach")
+     * @Route("Admin/Coach/new", name="newCoach")
      */
     public function newCoachAction(Request $request)
     {
